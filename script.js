c6146e5ecc0e67337f9ca68238450e7a5457967e
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-02-19 18:46:53 +0200

    .

---
+++ script.js
@@ -0,0 +1,81 @@
+// const x = 10;
+// const y = 5;
+
+// console.log(x + y);
+
+// console.log(x - y);
+
+// console.log(x * y);
+
+// console.log(x / y);
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x<z);
+// console.log(x<=z);
+// console.log(x === y);
+// console.log(x === z);
+
+// const valueA = '5';
+// console.log(Number(valueA)); 
+// console.log(typeof Number(valueA));
+
+
+// const valueB = 'random string';
+// console.log(Number(valueB)); 
+// console.log(typeof Number(valueB));
+
+
+// Парсить з рядка ціле число
+
+// console.log(Number.parseInt('5px')); 
+// console.log(Number.parseInt('12qwe74'));
+// console.log(Number.parseInt('12.46qwe79'));
+// console.log(Number.parseInt('qweqwe'));
+
+
+// Парсить з рядка дробове число
+
+// console.log(Number.parseFloat('5px')); 
+// console.log(Number.parseFloat('12qwe74'));
+// console.log(Number.parseFloat('12.46qwe79')); 
+// console.log(Number.parseFloat('qweqwe'));
+
+
+// console.log(0.1 + 0.2);
+
+
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+
+//  Math.floor(num) - повертає найбільше ціле число, менше, або дорівнює вказаному
+
+// console.log(Math.floor(1.7));
+
+// Math.ceil(num) - повертає найменше ціле число,
+// більше, або дорівнює зазначеному числу.
+
+// console.log(Math.ceil(1.2));
+
+// Math.round(num) - повертає значення числа, округлене до найближчого цілого
+
+// console.log(Math.round(1.3));
+
+// Math.max(num1, num2, ...) - повертає найбільше число з набору
+
+// console.log(Math.max(20, 10, 50, 40));
+
+
+// Math.min(num1, num2, ...) - повертає найменше число з набору
+
+console.log(Math.min(20, 10, 50, 40));
+
+
+// Math.min(num1, num2, ...) - повертає найменше число з набору
+
+console.log(Math.min(20, 10, 50, 40));
+
